9dee4e706fd6b1438ae4410fb262e33da71f1d3c
marcelmoldes <moldesmarcel41@gmail.com>
2024-04-05 16:38:18 +0200

    Tenants vallidatos updated

--- validators/tenantsValidator.js
+++ validators/tenantsValidator.js
@@ -4,8 +4,8 @@ module.exports = {
     name: Joi.string().alphanum().min(3).max(25),
     website: Joi.string().domain().required(),
     industry: Joi.string().min(3).max(100).required(),
-    annual_revenue: Joi.number().min(3).max(999999999).required(),
-    employees: Joi.number().min(3).max(10000).required()
+    annual_revenue: Joi.number().min(3).max(10).required(),
+    employees: Joi.number().min(3).max(10).required()
 
 }
 
